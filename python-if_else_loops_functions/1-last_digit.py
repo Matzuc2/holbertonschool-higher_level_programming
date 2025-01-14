b19a27d86ba4d0508951c00ff47f3678e87541e1
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-14 23:22:32 +0100

    i did not respect all the permissions, i corrected it

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ if number > 0:
 if number < 0:
     number2 = number % -10
 if number2 > 5:
-    print("Last digit of {} is {}".format(number, number2))
+    print("Last digit of {} is {} ".format(number, number2), end="")
     print("and is greater than five")
 elif number2 < 6:
-    print("Last digit of {} is {}".format(number, number2))
+    print("Last digit of {} is {} ".format(number, number2), end="")
     print("and is less than 6 and not 0")
 elif number == 0:
     print("Last digit of {} is {} and is 0".format(number, number2))
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     if i is not ord('e') and i is not ord('q'):
-        print(chr(i), end="")
+        print("{}".format(chr(i)), end="")
