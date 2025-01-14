fc1ff0fae72e7ae42167c774df5ad2ea7b6d9aff
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-14 23:58:26 +0100

    adding new correction of exercice 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
+        number = ord(i)-32
         if ord(i) >= 97:
-            print("{}".format(chr(ord(i)-32)), end="")
-        elif ord(i) < 97:
-            print("{}".format(i), end="")
+            i = chr(number)
+        print("{}".format(i), end="")
     print()
