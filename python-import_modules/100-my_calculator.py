026572014d820f3a2b003a73dad26c895ba37f10
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-15 20:38:46 +0100

    a

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -17,13 +17,21 @@ def main():
         return (1)
 
     if sys.argv[2] == "*":
-        print(mul(int(sys.argv[1]), int(sys.argv[3])))
+        result = mul(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} * {} = ".format(sys.argv[1], sys.argv[3]), end="")
+        print(result)
     elif sys.argv[2] == "+":
-        print(add(int(sys.argv[1]), int(sys.argv[3])))
+        result = add(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} + {} = ".format(sys.argv[1], sys.argv[3]), end="")
+        print(result)
     elif sys.argv[2] == "-":
-        print(sub(int(sys.argv[1]), int(sys.argv[3])))
+        result = sub(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} - {} = ".format(sys.argv[1], sys.argv[3]), end="")
+        print(result)
     elif sys.argv[2] == "/":
-        print(div(int(sys.argv[1]), int(sys.argv[3])))
+        result = div(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} / {} = ".format(sys.argv[1], sys.argv[3]), end="")
+        print(result)
 
 
 if __name__ == "__main__":
