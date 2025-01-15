1e58f6aadbd70da619e5818a7563d1980faf9415
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-15 17:00:24 +0100

    adding case where there is no argument

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -7,7 +7,7 @@ def main():
     if __name__ == '__main__':
         for i in range(1, len(sys.argv)):
             number += int(sys.argv[i])
-    if len(sys.argv) > 1:
+            
         print("{}".format(number))
 
 
