044555f342f7c776848ab3ba267cabb9f2f92265
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-15 16:27:48 +0100

    adding case where there is only 1 argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,9 @@ import sys
 def main():
 
     if len(sys.argv) == 1:
-        print("0 arguments.".format(len(sys.argv)))
+        print("0 arguments.")
+    if len(sys.argv) == 2:
+        print("1 argument:")
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
