5b8cfaffba0adb05abf9211c4ed2a492b6034749
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-15 15:18:51 +0100

    adding an if function to allow execution of function only in the main

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
+def main():
 
-a = 1
-b = 2
+    from add_0 import add
 
-print("{} + {}".format(a, b), end="")
-print(" = {}".format(add(a, b)))
+    a = 1
+    b = 2
+    if __name__ == '__main__':
+        print("{} + {} = {}".format(a, b, add(a, b)))
