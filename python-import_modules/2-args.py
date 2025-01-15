d16fdddec13aff9669b8d91936c91fa92edb90ed
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-15 16:24:33 +0100

    addin function to print number of arguments in python

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+
+    if len(sys.argv) == 1:
+        print("0 arguments.".format(len(sys.argv)))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
+
+
+main()
