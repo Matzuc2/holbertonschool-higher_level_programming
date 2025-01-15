0cc57a048e083da46d23f1ffe2914b5a5983898f
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-15 16:37:58 +0100

    correcting t he wrong cases

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,12 +4,12 @@ import sys
 
 def main():
     if __name__ == '__main__':
-        if len(sys.argv) == 1:
-            print("0 arguments.")
-        if len(sys.argv) == 2:
+        if len(sys.argv) > 2:
+            print("{} arguments:".format(len(sys.argv) - 1))
+        elif len(sys.argv) == 2:
             print("1 argument:")
         else:
-            print("{} arguments:".format(len(sys.argv) - 1))
+            print("{} arguments.".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
 
