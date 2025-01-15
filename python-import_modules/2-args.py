e7ecb3e6aff3576965f11dd1a5f62d586928bbb3
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-15 16:30:51 +0100

    adding condition to avoid importation

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,15 +3,15 @@ import sys
 
 
 def main():
-
-    if len(sys.argv) == 1:
-        print("0 arguments.")
-    if len(sys.argv) == 2:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    if __name__ == '__main__':
+        if len(sys.argv) == 1:
+            print("0 arguments.")
+        if len(sys.argv) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
 
 
 main()
