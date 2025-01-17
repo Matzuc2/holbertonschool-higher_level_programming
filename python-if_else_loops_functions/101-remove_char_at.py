0699260c81a9afe415af95d7af35418657dd072b
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-17 19:48:09 +0100

    adding advancde task to remove a lettter at a precise index in python

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if str == None:
+    if str == None or str == "":
         return (None)
     str1 = ""
-    if n != 0:
-        str1 = str[0]
-    else:
-        str1 = str[1]
-    for i in range(1, len(str)):
+    for i in range(len(str)):
         if i == n:
             continue
         str1 += str[i]
