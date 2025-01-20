a4b5bb7d3d7b125dbd47e4d0cc2163926ef1b714
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-20 22:15:22 +0100

    adding all corrections

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return(0, None)
+    if not sentence:
+        return (0, None)
     length = len(sentence)
     return (length, sentence[0])
