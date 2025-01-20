687060796d304e1692f9b2fb422740093920dd1a
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-20 21:53:16 +0100

    adding all corrections

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = list.copy(my_list)
     if idx < 0 or idx > len(my_list):
         return (new_list)
-    for i in (new_list):
+    for i in range(len(new_list)):
         if i == idx:
             new_list[i] = element
     return (new_list)
--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        first_char = None
-    else:
-        first_char = sentence[0]
+        return(0, None)
     length = len(sentence)
-    return (length, first_char)
+    return (length, sentence[0])
