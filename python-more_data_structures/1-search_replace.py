8a4fc23d480817be491798b4603737aebe5bb1e1
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-23 00:35:13 +0100

    correct task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = list.copy(my_list)
-    for i in new:
-        if i == search:
+    for i in range(len(new)):
+        if new[i] == search:
             new[i] = replace
     return (new)
