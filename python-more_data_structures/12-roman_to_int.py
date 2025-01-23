d7cef250f9c48cdaea7632fe1802a1d9135784d7
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-23 15:44:46 +0100

    adding 12 script

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or not roman_string or not str:
+        return 0
+    result = 0
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in range(len(roman_string)):
+        if roman_string[i] in d:
+            if i > 0 and d[roman_string[i]] > d[roman_string[i - 1]]:
+                result += (d[roman_string[i]] - (d[roman_string[i - 1]] * 2))
+            else:
+                result += d[roman_string[i]]
+    return (result)
