283c5bf64e23e1602baa830151723f687417eb58
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-31 16:24:09 +0100

    adding modification concerning the new function 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -22,7 +22,7 @@ class Square:
         """
         Calculates the area of the square.
         """
-        return (self.__size * self.__size)
+        return (self.__size ** 2)
 
     @property
     def position(self):
@@ -36,7 +36,9 @@ class Square:
         """
         Setter for the position attribute.
         """
-        if not isinstance(value, tuple) and len(value) != 2:
+        if not isinstance(value, tuple) or len(value) != 2 or type(value[0])\
+            is not int or value[0] < 0 or value[1] < 0\
+                or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
