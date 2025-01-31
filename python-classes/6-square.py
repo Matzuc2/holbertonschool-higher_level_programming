390ae141043be15d57a40788a7fe0e10c645c70e
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-31 16:45:14 +0100

    adding modification concerning the new function 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,8 +37,8 @@ class Square:
         Setter for the position attribute.
         """
         if not isinstance(value, tuple) or len(value) != 2 or type(value[0])\
-            is not int or value[0] < 0 or value[1] < 0\
-                or type(value[1]) is not int:
+            is not int or value[0] < 0 or type(value[1]) is not int\
+                or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
