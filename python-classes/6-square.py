19376e2195229d31c3771cef53e98c6738f90750
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-31 16:40:23 +0100

    adding modification concerning the new function 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -70,11 +70,11 @@ class Square:
             print()
             return
         else:
-            for h in range(self.__position[1]):
+            for _ in range(self.__position[1]):
                 print()
-            for i in range(0, self.__size):
-                for n in range(self.__position[0]):
+            for _ in range(self.__size):
+                for _ in range(self.__position[0]):
                     print(" ", end="")
-                for j in range(0, self.__size):
+                for _ in range(self.__size):
                     print('#', end="")
                 print()
