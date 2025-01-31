70bde6a199ec76bb3bd808303db2473f98add674
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-01-31 15:05:05 +0100

    adding modification concerning the new function 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -32,12 +32,8 @@ class Square:
              is not a tuple of 2 positive integers.
             ValueError: If size is negative.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """
