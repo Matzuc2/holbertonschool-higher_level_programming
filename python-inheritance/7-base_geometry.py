becb287623ebf6b729259d24afda88f3ee981959
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-02-04 17:12:28 +0100

    adding new tests to taks 1 and 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,43 +1,30 @@
 #!/usr/bin/python3
-"""
-Module: base_geometry
-This module defines the `BaseGeometry` class, which includes methods for
-geometric operations.
-"""
+"""Module that defines a base geometry class"""
 
 
 class BaseGeometry:
-    """
-    A base class for geometric operations.
-
-    This class provides methods for calculating area and validating
-    integer values.
-    """
+    """Defines methods for geometric shapes."""
 
     def area(self):
-        """
-        Raises an Exception indicating that the `area` method
-        is not implemented.
+        """Calculate the area of a geometric shape.
 
         Raises:
-            Exception: Always raises an exception with the message
-            "area() is not implemented".
+            Exception: if the area method is not implemented
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validates that `value` is a positive integer.
+        """Validate an integer value.
 
         Args:
-            name (str): The name of the parameter.
-            value (int): The value to validate.
+            name (str): the name of the value.
+            value (int): the value to validate.
 
         Raises:
-            TypeError: If `value` is not an integer.
-            ValueError: If `value` is less than or equal to 0.
+            TypeError: if the value is not an integer.
+            ValueError: if the value is less than or equal to 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
