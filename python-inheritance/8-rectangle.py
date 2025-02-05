c9e442a9818a5bcd71fa53242a0da623f1027500
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-02-05 22:19:55 +0100

    adding new tests to taks 1 and 7

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -40,7 +40,7 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is not greater than 0.
         """
-        if type(value) is not int and not isinstance(int, value):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
