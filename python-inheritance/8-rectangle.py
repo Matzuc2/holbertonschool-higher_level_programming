a4c75ad95ea0c1bb0ecd97a73e1d5e60183bf4f6
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-02-05 23:24:28 +0100

    renforce the confition for integer validator

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-"""
-Module: base_geometry
-This module defines the `BaseGeometry` class and the `Rectangle` class
-"""
+"""Module: base_geometry"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    A class used to represent a Rectangle, inheriting from `BaseGeometry`.
-    """
+    """A class used to represent a Rectangle, inheriting BaseGeometry"""
 
     def __init__(self, width, height):
-        """
-        Initializes the rectangle with width and height, validating them.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
+        """Initializes the rectangle with width and height, validating them."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
