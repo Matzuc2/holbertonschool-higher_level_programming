3e35ffdcf5ec9f2bd21a76da66852abe2174e97a
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-02-05 23:21:57 +0100

    adding doc modif for tesk 10

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -22,5 +22,6 @@ class Rectangle(BaseGeometry):
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        
         self.__width = width
         self.__height = height
