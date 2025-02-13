1a447d519a3c6c01641e6d3b5c53cd0757498819
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-02-13 20:17:46 +0100

    adding all files for task 9 completion

---
+++ python-input_output/9-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module defines the `Student` class.
+The class provides functionality to store student information
+and retrieve it as a dictionary representation.
+"""
+
+
+class Student:
+    """
+    Represents a student with a first name, last name, and age.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first name, last name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of the Student instance.
+
+        Returns:
+            dict: A dictionary containing the student's attributes.
+        """
+        return (self.__dict__)
