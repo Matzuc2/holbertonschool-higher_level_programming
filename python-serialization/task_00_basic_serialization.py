d1a5e812402ac176d7235057c5012844df0ba2f2
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-02-15 15:11:11 +0100

    adding all files for task 1 and readme

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import pickle
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "wb") as file:
+        pickle.dump(data, file)
+    
+
+def load_and_deserialize(filename):
+    with open(filename, "rb") as file:
+        loaded_data = pickle.load(file)
+    return loaded_data
