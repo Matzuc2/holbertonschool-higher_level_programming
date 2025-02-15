4dab4fb1b5cb3cf9f5fad023506576915e2399ce
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-02-15 15:28:16 +0100

    reduce nb of lines

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,38 +5,20 @@ Python objects using the pickle module.
 """
 
 import pickle
-import os
-import json
 
 
 def serialize_and_save_to_file(data, filename):
     """
     Serializes an object and saves it to a file.
 
-    Args:
-        data (any): The Python object to serialize.
-        filename (str): The name of the file to store the serialized data.
-
-    Returns:
-        None
     """
-    if os.path.exists(filename) is False:
-        with open(filename, "ab") as file:
-            pickle.dump(data, file)
-    else:
-        with open(filename, "wb") as file:
-            pickle.dump(data, file)
+    with open(filename, "wb") as file:
+        pickle.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """
     Loads and deserializes an object from a file.
-
-    Args:
-        filename (str): The name of the file containing the serialized data.
-
-    Returns:
-        any: The deserialized Python object.
     """
     with open(filename, "rb") as file:
         return pickle.load(file)
