dbdac93bd41963f24e479cd425080ad0cff0810d
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-02-15 15:13:35 +0100

    add all tasks

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
+"""
+This module provides functions for serializing and deserializing
+Python objects using the pickle module.
+"""
+
 import pickle
+
+
 def serialize_and_save_to_file(data, filename):
+    """
+    Serializes an object and saves it to a file.
+
+    Args:
+        data (any): The Python object to serialize.
+        filename (str): The name of the file to store the serialized data.
+
+    Returns:
+        None
+    """
     with open(filename, "wb") as file:
         pickle.dump(data, file)
-    
+
 
 def load_and_deserialize(filename):
+    """
+    Loads and deserializes an object from a file.
+
+    Args:
+        filename (str): The name of the file containing the serialized data.
+
+    Returns:
+        any: The deserialized Python object.
+    """
     with open(filename, "rb") as file:
         loaded_data = pickle.load(file)
     return loaded_data
