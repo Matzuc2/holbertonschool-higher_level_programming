30810e40037fc1fa047347fb28d94784fef51aac
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-02-15 15:31:02 +0100

    replaced picke by json cause json transforms data into bytes

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,21 +4,20 @@ This module provides functions for serializing and deserializing
 Python objects using the pickle module.
 """
 
-import pickle
-
+import json
 
 def serialize_and_save_to_file(data, filename):
     """
     Serializes an object and saves it to a file.
 
     """
-    with open(filename, "wb") as file:
-        pickle.dump(data, file)
+    with open(filename, "w") as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """
     Loads and deserializes an object from a file.
     """
-    with open(filename, "rb") as file:
-        return pickle.load(file)
+    with open(filename, "r") as file:
+        return json.load(file)
