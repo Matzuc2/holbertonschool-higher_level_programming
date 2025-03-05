4c04bdea835512bbac02cd82c48f39dcf66c8097
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-05 23:54:19 +0100

    create database and user for hbtn_0d_2; grant SELECT privilege to the user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- Creates database 'hbtn_0d_2', user 'user_0d_2'
---with password 'user_0d_2_pwd', and grants SELECT privilege on the database to the user.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
