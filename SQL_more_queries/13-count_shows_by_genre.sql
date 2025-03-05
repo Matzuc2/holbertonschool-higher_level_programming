614a503214b4847fb56584568aff7f8595d8aa02
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-05 23:58:19 +0100

    fix: improve SQL query for counting shows by genre with clearer joins

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- This script counts the number of TV shows by genre from the hbtn_0d_tvshows database.
-SELECT tv_genres.name as genre, COUNT(*) AS number_of_shows
-FROM tv_show_genres JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+SELECT tv_genres.name AS 'genre', COUNT(*) AS 'number_of_shows'
+FROM tv_genres 
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
