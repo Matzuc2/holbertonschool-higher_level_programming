b5b19ee5c9c379a54e95d610bf53d1d20a32f7d8
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-05 23:57:21 +0100

    fix: remove unnecessary line breaks in count shows by genre query

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- This script counts the number of TV shows by genre from the hbtn_0d_tvshows database.
 SELECT tv_genres.name as genre, COUNT(*) AS number_of_shows
-FROM tv_show_genres
-JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id
+FROM tv_show_genres JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 HAVING number_of_shows > 0
