fd4bd9a57617f870973f1ac8e4055eaed6007670
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-05 23:53:00 +0100

    update SQL queries to filter genres with shows and simplify database creation script

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,5 @@ FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_shows.id
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
