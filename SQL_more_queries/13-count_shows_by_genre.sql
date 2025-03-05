aa48aebe190ab7ffc51f3cd8f39ac11e0ae1fc27
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-05 23:56:11 +0100

    fix: correct GROUP BY clause in count shows by genre query

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT tv_genres.name as genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_shows.id
+GROUP BY tv_genres.name
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
