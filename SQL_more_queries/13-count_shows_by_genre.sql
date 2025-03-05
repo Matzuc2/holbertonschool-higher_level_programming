ba8d6f0a6f3bec3cda317a4f2ad4b7ce2b8a307b
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-05 23:55:35 +0100

    fix: standardize SQL keyword casing in count shows by genre query

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- This script counts the number of TV shows by genre from the hbtn_0d_tvshows database.
-SELECT tv_genres.name as genre, COUNT(*) as number_of_shows
+SELECT tv_genres.name as genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
