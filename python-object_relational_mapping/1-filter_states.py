0415c2dde63e961b47f7bbb1b08d8a352fe8c487
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-06 15:50:41 +0100

    fix: update docstring to clarify script functionality for listing states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Script that lists all states with a name starting with
-N from the database hbtn_0e_0_usa """
+""" Script that lists all states with a name starting with """
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
