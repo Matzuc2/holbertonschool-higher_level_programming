7de24cef2c2126f5e13f05163cdb03efa85e9c48
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-06 15:58:02 +0100

    fix: update SQL query to handle case sensitivity for state names starting with 'N'

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,7 +2,6 @@
 """ Script that lists all states with a name starting with """
 import MySQLdb
 import sys
-
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
@@ -10,7 +9,8 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE CONVERT(name USING Latin1) \
+        COLLATE Latin1_General_CS LIKE 'N%' ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
