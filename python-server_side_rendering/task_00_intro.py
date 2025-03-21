bb3b3a5c1f2878878c16bc8e6e545785fab94c7e
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-21 16:32:41 +0100

    fix: improve error messages and ensure proper string conversion in invitation generation

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,31 +1,36 @@
 import os
 
+
 def generate_invitations(template, attendees):
     if not isinstance(template, str):
         print("Error: Template must be a string.")
         return
-    if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
+    if not isinstance(attendees, list)\
+       or not all(isinstance(attendee, dict) for attendee in attendees):
         print("Error: Attendees must be a list of dictionaries.")
         return
     if template is None:
-        print("Error: Template dont have to be empty")
+        print("Error: Template don't have to be empty")
+        return
     if attendees is None:
-        print("Error: attendees dont have to be empty")
+        print("Error: Attendees don't have to be empty")
+        return
+
     generated_file = []
     for i, attendee in enumerate(attendees, start=1):
         try:
             template1 = template.replace("{name}", str(attendee.get("name", "N/A")))
             template1 = template1.replace("{event_title}", str(attendee.get("event_title", "N/A")))
             template1 = template1.replace("{event_date}", str(attendee.get("event_date", "N/A")))
-            template1 = template1.replace("{event_location}", attendee.get("event_location", "N/A"))
+            template1 = template1.replace("{event_location}", str(attendee.get("event_location", "N/A")))
             with open("output_{}.txt".format(i), "w") as f:
-                f.write(template1) 
+                f.write(template1)
         except KeyError as e:
-            print("Keyerror: {e}")
+            print(f"Keyerror: {e}")
 
-        
 
-            
+
+
 
 
 
