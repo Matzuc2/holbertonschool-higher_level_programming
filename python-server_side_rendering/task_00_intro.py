3b1bf10380de2f3f6aae3248341b19193c82e7c5
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-21 16:38:50 +0100

    refactor: improve readability of invitation generation by formatting template replacements

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -19,19 +19,19 @@ def generate_invitations(template, attendees):
     generated_file = []
     for i, attendee in enumerate(attendees, start=1):
         try:
-            template1 = template.replace("{name}", str(attendee.get("name", "N/A")))
-            template1 = template1.replace("{event_title}", str(attendee.get("event_title", "N/A")))
-            template1 = template1.replace("{event_date}", str(attendee.get("event_date", "N/A")))
-            template1 = template1.replace("{event_location}", str(attendee.get("event_location", "N/A")))
+            template1 = template.replace(
+                "{name}",
+                str(attendee.get("name", "N/A")))
+            template1 = template1.replace(
+                "{event_title}",
+                str(attendee.get("event_title", "N/A")))
+            template1 = template1.replace(
+                "{event_date}",
+                str(attendee.get("event_date", "N/A")))
+            template1 = template1.replace(
+                "{event_location}",
+                str(attendee.get("event_location", "N/A")))
             with open("output_{}.txt".format(i), "w") as f:
                 f.write(template1)
         except KeyError as e:
             print(f"Keyerror: {e}")
-
-
-
-
-
-
-
-
