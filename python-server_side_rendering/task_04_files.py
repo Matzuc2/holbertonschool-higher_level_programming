55572f3eed2368376a3df7f9cd6f0c731ee0a1dd
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-03-22 22:04:25 +0100

    feat: add database script to create and populate Products table

---
+++ python-server_side_rendering/task_04_files.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+import json
+import csv
+import sqlite3
+
+app = Flask(__name__)
+
+def read_json_file(filepath):
+    with open(filepath, 'r') as file:
+        return json.load(file)
+
+def read_csv_file(filepath):
+    products = []
+    with open(filepath, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            products.append({
+                "id": int(row["id"]),
+                "name": row["name"],
+                "category": row["category"],
+                "price": float(row["price"])
+            })
+    return products
+
+def read_sql_file(filepath):
+    connexion = sqlite3.connect(filepath)
+    cursor = connexion.execute("SELECT * FROM PRODUCTS")
+    rows = cursor.fetchall()
+    return rows
+
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    product_id = request.args.get('id', type=int)
+    
+    if source == 'json':
+        products = read_json_file('products.json')
+    elif source == 'csv':
+        products = read_csv_file('products.csv')
+    elif source == 'sql':
+        products = read_sql_file('products.db')
+    else:
+        return render_template('product_display.html', error="Wrong source")
+
+    if product_id:
+        products = [product for product in products if product['id'] == product_id]
+        if not products:
+            return render_template('product_display.html', error="Product not found")
+
+    return render_template('product_display.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
